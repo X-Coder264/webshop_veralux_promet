d7b92137e7757622399c696b1e31d849341aee68
Antonio <antonio.pauletich95@gmail.com>
2017-08-28 21:49:22 +0200

    Add user verification

---
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\RedirectsUsers;
+use Illuminate\Support\Facades\Validator;
+
+use Jrean\UserVerification\Facades\UserVerification;
+
+class VerificationController extends Controller
+{
+
+    use RedirectsUsers;
+
+    /**
+     * Where to redirect users after verification resend.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/login';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function getResendForm()
+    {
+        return view('auth.verification.email');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        $rules = [
+            'email' => 'required|email|max:255|exists:users,email',
+        ];
+
+        $messages = [
+            'email.required' => "E-mail je obavezno polje.",
+            'email.email' => "E-mail mora sadržavati strukturu oblika primjer@mail.com.",
+            'email.max' => "E-mail smije sadržavati maksimalno 255 znakova.",
+            'email.exists' => "Korisnik nije pronađen.",
+        ];
+
+        return Validator::make($data, $rules, $messages);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function resend(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        $user = User::where('email', $request->input('email'))->first();
+
+        if ($user->isVerified()) {
+            return redirect('/login')->with('success', 'Vaš korisnički račun je već aktiviran. Prijavite se u sustav');
+        }
+        
+        UserVerification::generate($user);
+        UserVerification::send($user, 'Aktivacija korisničkog računa', 'noreply@ahelos.hr', 'Ahelos');
+
+        return redirect($this->redirectPath())->with(
+            'warning',
+            'Poslana je aktivacijska poveznica na ' . $user['email'] .
+            '. Kliknite na dobivenu poveznicu kako biste aktivirali Vaš korisnički račun.'
+        );
+    }
+}
